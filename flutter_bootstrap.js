f458e43b419250c6506b4ae371aa4f1531bc0fcb
doutorizze-ux <doutorizze-ux@users.noreply.github.com>
2026-07-23 17:30:39 +0000

    deploy: cdf4f61c958e164e02b0ddff6bd3ec0c8cee2ddc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2891840207" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2036072897" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
